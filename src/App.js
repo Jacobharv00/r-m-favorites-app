bfbc4f300f139aa92c0e6a856fec9df87473477d
jacob <jacobharv00@gmail.com>
2021-07-08 15:44:18 -0600

    Example post request

--- src/App.js
+++ src/App.js
@@ -9,21 +9,32 @@ function App() {
   const [favorites, setFavorites] = useState([]);
 
   const addFavorite = ( character ) => {
-    const foundCharacter = favorites.find(char => char === character)
+    const foundCharacter = favorites.find( char => char === character )
       if (!foundCharacter) { // to make sure character doesn't get added multiple times
-      setFavorites([...favorites, character])
+        setFavorites( [ ...favorites, character ] )
+        // postFav(character)
     }
   }
 
+  // Example Post request
+  // const postFav = ( character ) => {
+  //   fetch( 'url', {
+  //     method: 'POST',
+  //     headers: 'application/json'
+  //   })
+  //   .then(resp => resp.json() )
+  //   .then(setFavorites([...favorites, character]))
+  // }
+
   const removeFavorite = ( character ) => {
-    const updatedFavorites = favorites.filter(favorite => favorite !== character) // give us a brand new list of the favorites who are not the character we are not trying to delete 
+    const updatedFavorites = favorites.filter(favorite => favorite !== character) // give us a brand new list of the favorites who is not the character we are not trying to delete 
     setFavorites(updatedFavorites)
   }
 
   useEffect(() => {
     fetch("https://rickandmortyapi.com/api/character/?page=2")
       .then((resp) => resp.json())
-      .then((data) => setCharacters(data.results));
+      .then((data) => setCharacters(data.results))
   }, [])
 
   return (
